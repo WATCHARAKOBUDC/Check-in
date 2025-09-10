57c5613f225ff0703b10fcb5c87a28655135d958
WATCHARAKOBUDC <WATCHARA.KO.BUDC@cpird.in.th>
2025-09-10 16:33:14 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwPYvMx3z811RjdEc4ywO_rbko5vq9NKfehSx13F4jGW-FxxEzxrM7NhgbV5meBnqn9/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbx2llMiV4qLRaJuTUFPrt8k4Ses9BEFvGs6a-jEeIw90H2P1wWw7vWjEMlPL-iKucckMg/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw0JXdjDl-KT4iwF5TPp2fjryddfUMxJfxMYqoZXcW1hzPtsDGGrATlHvBCbY3hA7XjNg/exec';
 //LIFF_ID
     const LIFF_ID = '2008088006-qymDj33d'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008088006-PVgWdzzn'; // LIFF ID history
     const LIFF_REGISTER = '2008088006-d90lQ11p'; // LIFF ID register
+
 
